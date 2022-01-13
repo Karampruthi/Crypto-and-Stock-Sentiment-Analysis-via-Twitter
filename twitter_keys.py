23a25d70b445ec2c91efa625938288d2d6ea381d
karampruthi <karampruthi@gmail.com>
2022-01-13 21:32:53 +0000

    Initial commit

---
+++ twitter_keys.py
@@ -0,0 +1,4 @@
+api_key = 'AiKyp0QZj1Za8Kbe7H6BivRqh'
+api_key_secret = 'CJJe9cRIjl8knBjF46wjVFSOtkzBz0CFNsKcmpf9lJE3yRPqV7'
+access_token = '111656626-wWjZWPv3utSSAtFQLrrFYo3wEEWGk1rcndtZi07E'
+access_token_secret = 'm05vVqDRYMyP3AvFLt1qsQNXwqkjJr56Gh3xDclnMdYOi'
